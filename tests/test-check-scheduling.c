325714ebb2ab2a9df235a43f910a0ec2cb0398fd
Jacob Hansen <jhansen@op5.com>
2018-07-09 14:12:15 +0000

    Tests: Always use host retry_interval
    Currently, when a service is in a HARD CRITICAL it instantly trigges a
    host-check. As a result the host retry_interval is basicly useless when
    a host (and presumebly therefore its services) is down.
    
    This patch adds a test to ensure that a host uses its own retry_interval
    even when it has a service in HARD CRITICAL.
    
    This is part of MON-5625
    
    Signed-off-by: Jacob Hansen <jhansen@op5.com>

--- tests/test-check-scheduling.c
+++ tests/test-check-scheduling.c
@@ -380,6 +380,68 @@ START_TEST(test_check_window)
 }
 END_TEST
 
+
+/*
+ * Ensure that host checks uses their own retry_interval rather than being
+ * rescheduled due to the service being in a HARD CRITICAL.
+ */
+START_TEST(host_retry_interval_on_service_down)
+{
+	struct nm_event_execution_properties ep = {
+		.execution_type = EVENT_EXEC_NORMAL,
+		.event_type = EVENT_TYPE_TIMED,
+		.user_data = hst
+	};
+	struct nm_event_execution_properties ep2 = {
+		.execution_type = EVENT_EXEC_NORMAL,
+		.event_type = EVENT_TYPE_TIMED,
+		.user_data = svc
+	};
+	long actual_time_left;
+	time_t expected_time_left;
+	check_result cr, cr2;
+
+	hst->check_options = 0;
+	hst->checks_enabled = TRUE;
+	hst->check_freshness = TRUE;
+	hst->max_attempts = 3;
+	hst->check_interval = 5.0;
+	hst->retry_interval = 30.0;
+	hst->current_attempt = 2;
+
+	init_check_result(&cr);
+	cr.object_check_type = HOST_CHECK;
+	cr.check_type = CHECK_TYPE_ACTIVE;
+	cr.return_code = STATE_CRITICAL;
+	handle_host_check_event(&ep);
+  handle_async_host_check_result(hst, &cr);
+
+	svc->check_options = 0;
+	svc->checks_enabled = TRUE;
+	svc->check_freshness = TRUE;
+	svc->max_attempts = 3;
+	svc->check_interval = 5.0;
+	svc->retry_interval = 5.0;
+	svc->current_state = STATE_CRITICAL;
+	svc->last_hard_state = STATE_CRITICAL;
+	svc->state_type = HARD_STATE;
+	svc->current_attempt = 3;
+	svc->host_ptr = hst;
+
+	init_check_result(&cr2);
+	cr2.object_check_type = SERVICE_CHECK;
+	cr2.check_type = CHECK_TYPE_ACTIVE;
+	cr2.return_code = STATE_CRITICAL;
+	handle_service_check_event(&ep2);
+	handle_async_service_check_result(svc, &cr);
+
+	actual_time_left = get_timed_event_time_left_ms(hst->next_check_event);
+	expected_time_left = get_host_retry_interval_s(hst) * 1000;
+	printf("actual: %lld expected: %lld\n", (long long) actual_time_left, (long long) expected_time_left);
+	assert_approximately_equal(expected_time_left, actual_time_left, APPROXIMATION_TOLERANCE_MS);
+}
+END_TEST
+
 Suite*
 check_scheduling_suite(void)
 {
@@ -400,6 +462,7 @@ check_scheduling_suite(void)
 	tcase_add_test(tc_intervals, host_check_interval_OK_states);
 	tcase_add_test(tc_intervals, host_retry_interval_soft_non_OK_states);
 	tcase_add_test(tc_intervals, host_check_interval_hard_non_OK_states);
+	tcase_add_test(tc_intervals, host_retry_interval_on_service_down);
 	suite_add_tcase(s, tc_intervals);
 
 	tcase_add_checked_fixture(tc_miscellaneous, setup, teardown);
